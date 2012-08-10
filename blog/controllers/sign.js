f097a100d9ce7e7cc636316e00bc58c0a615c660
wolongxzg <wolongxzg@gmail.com>
2012-08-10 18:20:31 +0800

    update

--- blog/controllers/sign.js
+++ blog/controllers/sign.js
@@ -1,6 +1,5 @@
 var sanitize = require('validator').sanitize;
-
-// var crypto = require('crypto');
+var crypto = require('crypto');
 var config = require('../config').config;
 
 
@@ -11,20 +10,8 @@ var config = require('../config').config;
  * @param  {HttpResponse} res
  */
 exports.showLogin = function(req, res) {
-    //req.session._loginReferer = req.headers.referer;
     res.render('sign/signin');
 };
-
-
-/**
- * define some page when login just jump to the home page
- * @type {Array}
- */
-// var notJump = ['/active_account', //active page
-// '/reset_pass', //reset password page, avoid to reset twice
-// '/signup', //regist page
-// '/search_pass' //serch pass page
-// ];
 
 /**
  * Handle user login.
@@ -49,10 +36,9 @@ exports.login = function(req, res, next) {
     //     if (err) return next(err);
     //     if (!user) {
     //         return res.render('sign/signin', {
-    //             error: '这个用户不存在。'
+    //             error: '用户不存在。'
     //         });
     //     }
-    //     pass = md5(pass);
     //     if (pass !== user.pass) {
     //         return res.render('sign/signin', {
     //             error: '密码错误。'
@@ -60,15 +46,6 @@ exports.login = function(req, res, next) {
     //     }
     //     // store session cookie
     //     gen_session(user, res);
-    //     //check at some page just jump to home page
-    //     var refer = req.session._loginReferer || 'home';
-    //     for (var i = 0, len = notJump.length; i != len; ++i) {
-    //         if (refer.indexOf(notJump[i]) >= 0) {
-    //             refer = 'home';
-    //             break;
-    //         }
-    //     }
-    //     res.redirect(refer);
     // });
 };
 
